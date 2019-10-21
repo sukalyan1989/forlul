b437336bf9cf58b9acba308c8b529311f477abb5
unknown <sukalyan1989@gmail.com>
2019-10-22 00:10:38 +0530

    none

--- app.py
+++ app.py
@@ -4,9 +4,13 @@ Created on Sun Jul 14 19:27:56 2019
 
 @author: sukalyan
 """
-
+import pandas as pd
+import numpy as np
+import requests as rq
+import json
+import getpass
 import os
-from flask import Flask, flash, request, redirect, url_for, send_from_directory,render_template
+from flask import Flask, flash, request, redirect, url_for, send_from_directory,render_template,send_file
 
 from werkzeug.utils import secure_filename
 
@@ -44,8 +48,54 @@ def uploaded_file(filename):
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@app.route('/lul')
+def lul():
+    uname="sharepoint_jade"
+    jade_url="https://jade.jhpiego.org/api/categoryOptions?filter=name:like:PRJ&fields=name,id,categoryOptionGroups[name,id,code]&paging=false"
+
+    login=(uname,"Tu!2NQVnRxzhQAmmMt")
+
+    dtable=rq.get(jade_url,auth=login)
+
+    d1=json.loads(dtable.text)
+    df=pd.DataFrame.from_dict(d1)
+    test = df["categoryOptions"].apply(awdprj).apply(pd.Series)
+    test.to_csv(os.path.join(app.config['UPLOAD_FOLDER'])+'/data.CSV',index=False)
+    return send_from_directory(app.config['UPLOAD_FOLDER'],'data.CSV')
+
+
+
+
+def awdprj(args):
+    awd = {}
+    prj = {}
+    grp = {
+        "id": args["id"],
+        "name": args["name"]
+    }
+    if not "categoryOptionGroups" in args:
+        return grp
+    if len(args["categoryOptionGroups"]) == 0:
+        return grp
+    if "AWD" in args["categoryOptionGroups"][0]["name"]:
+        awd = args["categoryOptionGroups"][0]
+        prj = args["categoryOptionGroups"][1]
+    else:
+        prj = args["categoryOptionGroups"][0]
+        awd = args["categoryOptionGroups"][1]
+    grp["AWD_id"] = awd["id"]
+    grp["AWD_name"] = awd["name"]
+    grp["AWD_code"] = awd["code"]
+    grp["PRJ_id"] = prj["id"]
+    grp["PRJ_name"] = prj["name"]
+    return grp
+
+
+#Create pd
+
+    #Export to CSV
 
 
 if __name__=='__main__':
-   # app.debug=True
-    app.run()
+   app.debug=True
+   app.run()

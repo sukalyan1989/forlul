311d39075c69ffc1a5371cb323e555ac2f35a7cc
unknown <sukalyan1989@gmail.com>
2019-10-24 00:07:25 +0530

    none

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import json
 import getpass
 import os
 from flask import Flask, flash, request, redirect, url_for, send_from_directory,render_template,send_file
-
+import warnings
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = os.getcwd()+'/Uploads'
@@ -50,52 +50,49 @@ def allowed_file(filename):
 
 @app.route('/lul',methods=['POST'])
 def lul():
+    warnings.filterwarnings('ignore')
     uname="sharepoint_jade"
-    # print(request.get_json(force=True)['name'])
-    jade_url=request.get_json(force=True)['name']
-    # jade_url="https://jade.jhpiego.org/api/categoryOptions?filter=name:like:PRJ&fields=name,id,categoryOptionGroups[name,id,code]&paging=false"
+    jade_url="https://jade.jhpiego.org/api/categoryOptions?filter=name:like:PRJ&fields=name,id,categoryOptionGroups[name,id,code]&paging=false"
 
     login=(uname,"Tu!2NQVnRxzhQAmmMt")
 
     dtable=rq.get(jade_url,auth=login)
 
     d1=json.loads(dtable.text)
-    df=pd.DataFrame.from_dict(d1)
-    test = df["categoryOptions"].apply(awdprj).apply(pd.Series)
-    test.to_csv(os.path.join(app.config['UPLOAD_FOLDER'])+'/data.CSV',index=False)
+
+    #Create pd
+    df=pd.DataFrame.from_dict(d1, orient='columns')
+    df=df['categoryOptions'].apply(pd.Series)
+    #Flatten category options groups
+    df_p=df['categoryOptionGroups'].apply(pd.Series)
+    #df_p.columns=['cat','sec','third']
+
+    #print (df_p)
+    df_cat=df_p[0].apply(pd.Series)
+    df_cat.rename(columns={'name':'COG_1_name','id':'COG_1_id','code':'COG_1_code'},inplace=True)
+    df_cat2=df_p[1].apply(pd.Series)
+    df_cat2.rename(columns={'name':'COG_2_name','id':'COG_2_id','code':'COG_2_code'},inplace=True)
+    df_cat3=df_p[2].apply(pd.Series)
+    df_cat3.rename(columns={'name':'COG_3_name','id':'COG_3_id','code':'COG_3_code'},inplace=True)
+
+    #Combine all
+    final_table=pd.concat([df,df_cat,df_cat2,df_cat3],axis=1)
+    final_table=final_table.drop(['categoryOptionGroups',0],axis=1)
+
+    final_table['COG.PRJ_name']=np.where(final_table.COG_1_name.str.contains('PRJ'),final_table.COG_1_name,np.where(final_table.COG_2_name.str.contains('PRJ'),final_table.COG_2_name,''))
+    final_table['COG.PRJ_id']=np.where(final_table.COG_1_name.str.contains('PRJ'),final_table.COG_1_id,np.where(final_table.COG_2_name.str.contains('PRJ'),final_table.COG_2_id,''))
+    final_table['COG.PRJ_code']=np.where(final_table.COG_1_name.str.contains('PRJ'),final_table.COG_1_code,np.where(final_table.COG_2_name.str.contains('PRJ'),final_table.COG_2_code,''))
+
+
+    final_table['COG.AWD_name']=np.where(final_table.COG_1_name.str.contains('AWD'),final_table.COG_1_name,np.where(final_table.COG_2_name.str.contains('AWD'),final_table.COG_2_name,''))
+    final_table['COG.AWD_id']=np.where(final_table.COG_1_name.str.contains('AWD'),final_table.COG_1_id,np.where(final_table.COG_2_name.str.contains('AWD'),final_table.COG_2_id,''))
+    final_table['COG.AWD_code']=np.where(final_table.COG_1_name.str.contains('AWD'),final_table.COG_1_code,np.where(final_table.COG_2_name.str.contains('AWD'),final_table.COG_2_code,''))
+
+
+    final_table=final_table.loc[:,['name','id','COG.PRJ_name','COG.PRJ_id','COG.PRJ_code','COG.AWD_name','COG.AWD_id','COG.AWD_code']]
+    final_table.to_csv(os.path.join(app.config['UPLOAD_FOLDER'])+'/data.CSV',index=False)
     return send_from_directory(app.config['UPLOAD_FOLDER'],'data.CSV')
 
-
-
-
-def awdprj(args):
-    awd = {}
-    prj = {}
-    grp = {
-        "id": args["id"],
-        "name": args["name"]
-    }
-    if not "categoryOptionGroups" in args:
-        return grp
-    if len(args["categoryOptionGroups"]) == 0:
-        return grp
-    if "AWD" in args["categoryOptionGroups"][0]["name"]:
-        awd = args["categoryOptionGroups"][0]
-        prj = args["categoryOptionGroups"][1]
-    else:
-        prj = args["categoryOptionGroups"][0]
-        awd = args["categoryOptionGroups"][1]
-    grp["AWD_id"] = awd["id"]
-    grp["AWD_name"] = awd["name"]
-    grp["AWD_code"] = awd["code"]
-    grp["PRJ_id"] = prj["id"]
-    grp["PRJ_name"] = prj["name"]
-    return grp
-
-
-#Create pd
-
-    #Export to CSV
 
 
 if __name__=='__main__':
